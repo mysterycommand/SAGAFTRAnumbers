f247958f9e476d59bd8a4bef8f437c6345d30a63
Matt Hayes <matt@mysterycommand.com>
2016-07-16 17:23:21 -0700

    Updates July 16, 2016 (manually, looks like Travis config is borked). [ci skip]

---
+++ scripts/6a584879.rates.js
@@ -0,0 +1,158 @@
+(function() {
+  var root;
+
+  root = this;
+
+  root.sag_aftra_rates = {
+    radio_commercial: {
+      session_actor: 298.10,
+      session_singer: 298.10,
+      tag_2_25: 115.30,
+      tag_26_50: 82.75,
+      tag_51: 45.15,
+      wild_13_unit_2_25: 4.39,
+      wild_13_unit_26: 3.30,
+      wild_13_major_new_york: 446.35,
+      wild_13_major_chicago: 404.83,
+      wild_13_major_los_angeles: 404.83,
+      wild_13_major_any_2: 544.42,
+      wild_13_major_all_3: 687.90,
+      wild_8_unit_2_25: 3.51,
+      wild_8_unit_26: 2.64,
+      wild_8_major_new_york: 357.06,
+      wild_8_major_chicago: 323.89,
+      wild_8_major_los_angeles: 323.89,
+      wild_8_major_any_2: 435.54,
+      wild_8_major_all_3: 550.30,
+      network_1: 504.45,
+      network_4: 818.39,
+      network_8: 1303.63,
+      network_13: 1617.73,
+      network_13_limited_26: 808.92,
+      network_13_limited_39: 1218.20,
+      regional_network: 976.21,
+      dealer_actor: 806.19,
+      dealer_singer: 597.70,
+      internet_8_week: 396.47,
+      internet_1_year: 1043.35
+    },
+    radio_commercial_demo: {
+      session_actor: 205.44,
+      session_singer: 205.44
+    },
+    television_commercial_on_camera: {
+      session_actor: 671.69,
+      session_extra: 366.37,
+      actor_wardrobe: 83.96,
+      extra_wardrobe: 83.96,
+      wild_13_unit_2_25: 22.99,
+      wild_13_unit_26: 8.53,
+      wild_13_major_new_york: 1320.01,
+      wild_13_major_chicago: 1150.57,
+      wild_13_major_los_angeles: 1150.57,
+      wild_13_major_any_2: 1816.54,
+      wild_13_major_all_3: 2191.09,
+      wild_13_major_all_3_addl: 8.17,
+      local_1_50k: 29.64,
+      local_50k_100k: 59.60,
+      local_100k_150k: 89.24,
+      local_150k_200k: 119.04,
+      local_200k_250k: 148.68,
+      local_250k_500k: 297.62,
+      local_500k_750k: 446.30,
+      local_750k_1m: 595.08,
+      national_min: 671.69,
+      national_max: 3906.00,
+      network_2: 157.11,
+      network_3: 124.64,
+      network_4_13: 124.64,
+      internet_8_week: 1007.54,
+      internet_1_year: 2518.84
+    },
+    television_commercial_off_camera: {
+      session_actor: 505.04,
+      tag_2_25: 150.23,
+      tag_26_50: 83.35,
+      tag_51: 45.42,
+      wild_13_unit_2_25: 15.73,
+      wild_13_unit_26: 6.69,
+      wild_13_major_new_york: 932.56,
+      wild_13_major_chicago: 811.49,
+      wild_13_major_los_angeles: 811.49,
+      wild_13_major_any_2: 1223.12,
+      wild_13_major_all_3: 1556.26,
+      wild_13_major_all_3_addl: 6.40,
+      local_1_50k: 20.22,
+      local_50k_100k: 40.71,
+      local_100k_150k: 61.10,
+      local_150k_200k: 81.48,
+      local_200k_250k: 101.81,
+      local_250k_500k: 203.78,
+      local_500k_750k: 305.54,
+      local_750k_1m: 407.46,
+      national_min: 505.04,
+      national_max: 2618.50,
+      network_2: 122.95,
+      network_3: 97.79,
+      network_4_13: 97.79,
+      internet_8_week: 757.56,
+      internet_1_year: 1893.90
+    },
+    television_commercial_demo: {
+      session_actor: 252.55
+    },
+    psa_radio: {
+      session_actor: 674.47,
+      session_singer: 674.47
+    },
+    psa_television_on_camera: {
+      session_actor: 671.69,
+      session_extra: 366.37,
+      actor_wardrobe: 83.96,
+      extra_wardrobe: 83.96
+    },
+    psa_television_off_camera: {
+      session_actor: 505.04
+    },
+    corp_edu_on_camera: {
+      cat_1_session_narrator_half_day: 504.50,
+      cat_1_session_narrator_day_1: 891.00,
+      cat_1_session_narrator_day_2: 490.00,
+      cat_1_session_player_half_day: 318.50,
+      cat_1_session_player_day_1: 490.00,
+      cat_1_session_player_day_3: 1233.00,
+      cat_1_session_player_week: 1721.50,
+      cat_1_session_extra_general: 127.50,
+      cat_1_session_extra_special: 140.50,
+      cat_1_session_extra_silent: 238.00,
+      cat_2_session_narrator_day_1: 1056.00,
+      cat_2_session_narrator_day_2: 609.50,
+      cat_2_session_player_half_day: 396.00,
+      cat_2_session_player_day_1: 609.50,
+      cat_2_session_player_day_3: 1520.50,
+      cat_2_session_player_week: 2131.50,
+      cat_2_session_extra_general: 127.50,
+      cat_2_session_extra_special: 140.50,
+      cat_2_session_extra_silent: 238.00
+    },
+    corp_edu_off_camera: {
+      cat_1_session_actor_first_hour: 401.00,
+      cat_1_session_actor_addl_half: 117.50,
+      cat_2_session_actor_first_hour: 446.50,
+      cat_2_session_actor_addl_half: 117.50
+    },
+    corp_edu_audio_only: {
+      cat_1_session_actor_first_hour: 401.00,
+      cat_1_session_actor_addl_half: 117.50,
+      cat_2_session_actor_first_hour: 446.50,
+      cat_2_session_actor_addl_half: 117.50,
+      ivr_session_actor_first_hour: 223.00,
+      ivr_session_actor_addl_half: 111.00,
+      store_3_month_session_actor_first_hour: 401.00,
+      store_3_month_session_actor_addl_half: 117.50,
+      store_6_month_session_actor_first_hour: 802.00,
+      store_6_month_session_actor_addl_half: 117.50
+    }
+  };
+
+}).call(this);
